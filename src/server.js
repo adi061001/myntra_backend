46f99a6192fe6209740a1b7580dc68b31413771d
adi061001 <adityasharma@thoughtwin.com>
2026-02-04 15:50:44 +0530

    resolve corss error

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import app from "./app.js";
+import mongoose from "mongoose";
 
-const app = require("./app");
-const connectDB = require("./config/db");
+dotenv.config();
 
-connectDB();
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
+// server start
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port 5000");
 });
