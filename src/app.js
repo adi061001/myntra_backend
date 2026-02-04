1a38fd2acaec4b7e444af48594056b234760452b
adi061001 <adityasharma@thoughtwin.com>
2026-02-04 15:50:44 +0530

    resolve corss error

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
-const express = require("express");
-const authRoutes = require("./routes/auth.routes");
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes.js"; // 🔥 import routes
 
 const app = express();
 
-app.use(express.json()); // read JSON body
+app.use(express.json());
 
+app.use(cors({
+  origin: [
+    "http://localhost:5174",
+    "https://your-frontend.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+// routes
 app.use("/api/auth", authRoutes);
 
-module.exports = app;
+export default app;
